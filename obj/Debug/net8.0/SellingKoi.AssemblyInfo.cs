03ac2e95a28fcc8c007294ba74345d8fc19c6a8d
NamFeng247 <phongldnse173682@fpt.edu.vn>
2024-10-11 22:24:06 +0700

    Change layout

--- obj/Debug/net8.0/SellingKoi.AssemblyInfo.cs
+++ obj/Debug/net8.0/SellingKoi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SellingKoi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a4d195d4ab3cab390079cd60670c6cd8c7b2134")]
 [assembly: System.Reflection.AssemblyProductAttribute("SellingKoi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SellingKoi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
